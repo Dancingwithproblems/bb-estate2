51465c4330879a500467e0dfc9136ca44548195d
BoatBoat <bbdev.mm@gmail.com>
2023-12-06 16:01:24 +0630

    add delete user functionlity

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -29,3 +29,15 @@ res.status(200).json(rest);
 }
 
 };
+
+export const deleteUser = async (req, res, next) => {
+    console.log(req);
+    console.log(res);
+    if(req.user.id !== req.params.id) return next(errorHandler(401, 'You can only delete your own account!'))
+    try {
+await User.findByIdAndDelete(req.params.id)
+res.status(200).json('User has been deleted!')
+} catch (error) {
+    next(error)
+}
+}
