fea7e21819b4602de08d6235ad86a31492b2a8d4
BoatBoat <bbdev.mm@gmail.com>
2023-12-07 10:58:10 +0630

    add sign out functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -48,6 +48,19 @@ const userSlice = createSlice ({
             state.error = action.payload;
             state.loading = false;
         },
+        signOutUserStart: (state) => {
+            state.loading = true;
+        },
+    signOutUserSuccess: (state) => {
+        state.currentUser = null;
+        state.loading = false;
+        state.error = null;
+        
+    },
+    signOutUserFailure: (state, action) => {
+        state.error = action.payload;
+        state.loading = false;
+    },
 
     },
 });
@@ -57,6 +70,9 @@ updateUserFailure, updateUserSuccess, updateUserStart,
 deleteUserFailure,
 deleteUserSuccess,
 deleteUserStart,
+signOutUserFailure,
+signOutUserStart,
+signOutUserSuccess
 } = userSlice.actions;
 
 export default userSlice.reducer;
