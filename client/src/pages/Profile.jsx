d2c8955c4e10dcb6405ef3efc5245e5e80d03f71
BoatBoat <bbdev.mm@gmail.com>
2023-12-21 10:26:06 +0630

    Complete show user listings functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -16,6 +16,8 @@ export default function Profile() {
     (false);
     const [formData, setFormData] = useState({ });
     const [updateSuccess, setUpdateSuccess] = useState(false);
+    const [showListingsError, setShowListingError] = useState(false)
+    const [userListings, setUserListings] = useState([]);
     const dispatch = useDispatch();
 
     console.log(formData);
@@ -127,6 +129,24 @@ export default function Profile() {
         }
 
     }
+    const handleShowListings = async () => {
+        try {
+            console.log("ပူတူး");
+            setShowListingError(false);
+            const res = await fetch(`/api/user/listings/${currentUser._id}`);
+            const data = await res.json();
+            if (data.success === false) {
+                setShowListingError(true);
+                return;
+            }
+            console.log('data',data)
+            setUserListings(data);
+
+        } catch (error) {
+            showListingsError(true);
+
+        }
+    }
 
     return (
         <div className="p-3 max-w-lg mx-auto">
@@ -175,6 +195,35 @@ export default function Profile() {
             <p className='text-green-700 mt-5'>
                 {updateSuccess ? 'User is update successfully!' : ''}
             </p>
+            <button onClick={handleShowListings} className='text-green-700 w-full'>Show Listings</button>
+            <p className='text-red-700 mt-5'>{showListingsError ? 'Error showing listings': ''}</p>
+                {
+                userListings && 
+                userListings.length > 0 &&
+                <div className='flex flex-col gap=4'>
+                    <h1 className='text-center mt-7 text-2xl font-semibold
+                    '>Your Listings</h1>
+               { userListings.map((listing) => (
+                    <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center gap-4'>
+                        <Link to={`/listing/${listing._id}`}>
+                            <img src={listing.imageUrls[0]}
+                             alt="listing cover" className='h-16 w-16 object-contain rounded-lg'/>
+                        </Link>
+                        <Link className='text-slate-700 font-semibold hover:underline truncate flex-1' to={`/listing/${listing._id}`}>
+                            <p>
+                                
+                                {listing.name}</p>
+                        </Link>
+                        <div className='flex flex-col item-center'>
+                            <button className='text-red-700 uppercase'>Delete</button>
+                            <button className='text-green-700 uppercase'>Edit</button>
+
+                        </div> 
+                    </div>
+                ))}
+                </div>
+                }
+        
         </div>
     )
 }
