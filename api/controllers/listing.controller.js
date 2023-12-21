30af5369e966a07b6b81296d36dc22535503f216
BoatBoat <bbdev.mm@gmail.com>
2023-12-21 12:43:40 +0630

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -2,6 +2,7 @@ import Listing from '../models/listing.model.js';
 import { errorHandler } from '../utils/error.js';
 
 
+
 export const createListing = async (req, res, next) => {
   try {
     const listing = await Listing.create(req.body);
@@ -28,5 +29,29 @@ export const deleteListing = async (req, res, next) => {
 
   } catch (error) {
     next(error);
+  }
+}
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, 'Listing not found!'));
+  }
+
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, 'You can only update your own litings!'));
+  }
+
+  try {
+    const  updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true}
+    );
+
+    res.status(200).json(updatedListing);
+
+  } catch(error) {
+    next(error)
   }
 }
